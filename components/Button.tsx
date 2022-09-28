89b0003532be8f30ca7db683563c0fd3cff56d05
Mark Jay Lunas <you@exagit config --global user.email markjay.lunas@gmail.com git config --global user.email you@exa>
2022-09-28 19:52:52 +0800

    initial commit

---
+++ components/Button.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+const Button: React.FC = () => {
+    const [color, setColor] = useState('rgb(255,255,0)');
+    const [doubleSize, setdoubleSize] = useState(1);
+
+    const random = () => Math.floor(Math.random() * 255);
+    const onClick = () => {
+        setColor(`rgb(${random()},${random()},${random()})`);
+        setdoubleSize((prevSize) => prevSize * 2);
+    };
+    const style = {
+        backgroundColor: color,
+        transform: `scale(${doubleSize})`,
+    };
+    return (
+        <button
+            className="text-white rounded-md border-2 border-white px-4 py-1 "
+            onClick={onClick}
+            style={style}
+        >
+            Change
+        </button>
+    );
+};
+
+export default Button;
