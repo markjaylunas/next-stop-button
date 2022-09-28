4b0ec6452d065e0c15f165b7f511f4260f87a7cc
Mark Jay Lunas <you@exagit config --global user.email markjay.lunas@gmail.com git config --global user.email you@exa>
2022-09-28 19:55:30 +0800

    change button text to STOP

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 const Button: React.FC = () => {
-    const [color, setColor] = useState('rgb(255,255,0)');
+    const [color, setColor] = useState('#00b3ff');
     const [doubleSize, setdoubleSize] = useState(1);
 
     const random = () => Math.floor(Math.random() * 255);
@@ -15,11 +15,11 @@ const Button: React.FC = () => {
     };
     return (
         <button
-            className="text-white rounded-md border-2 border-white px-4 py-1 "
+            className="text-white rounded-md border-2 border-white px-4 py-1 transition ease-in-out"
             onClick={onClick}
             style={style}
         >
-            Change
+            STOP
         </button>
     );
 };
